8f2503ac50b3099865b1c2f8e75316da86e96996
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-11-21 15:38:28 +0100

    Changed styles to match main website

--- web/config/authservice.js
+++ web/config/authservice.js
@@ -1,43 +1,46 @@
-const { hashSync, compareSync } = require('bcrypt');
+const { hashSync, compareSync } = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 class Authservice {
     static validatePasswords = (password, user_password) => {
         return compareSync(password, user_password);
-    }
+    };
 
     static issueJWT = (user) => {
-        const expiresIn = '2h';
+        const expiresIn = "2h";
         delete user.password;
         const payload = {
-            user: user
+            user: user,
         };
 
-        const signedToken = jwt.sign(payload, process.env.APP_SECRET, { expiresIn: expiresIn, algorithm: 'HS256' });
+        const signedToken = jwt.sign(payload, process.env.APP_SECRET, {
+            expiresIn: expiresIn,
+            algorithm: "HS256",
+        });
 
         return {
             token: signedToken,
-            expires: expiresIn
-        }
-    }
+            expires: expiresIn,
+        };
+    };
 
     static decodeJWT = (token) => {
         const decoded = jwt.verify(token, process.env.APP_SECRET);
         return decoded.user;
-    }
+    };
 
     static comparePasswords = (password, password_rp) => {
         return password === password_rp;
-    }
+    };
 
     static hashPassword = (password) => {
-        return hashSync(password, 10)
-    }
+        return hashSync(password, 10);
+    };
 
     static getSafeData = (user) => {
         delete user.password;
         return user;
-    }
+    };
 }
 
 module.exports = Authservice;
