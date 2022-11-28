83d2f9f44afceb0eb4f2667d4aba071b46c1a9dc
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-11-28 20:23:48 +0100

    Reverted authetication changes

--- web/middleware/auth.js
+++ web/middleware/auth.js
@@ -2,33 +2,9 @@ const authentication = require("../config/passport");
 const passport = authentication.passport;
 const jwt = require("jsonwebtoken");
 
-class Auth {
-    static authenticate = passport.authenticate("jwt-authentication", {
-        session: false,
-        failureRedirect: '/login'
-    });
+const auth = passport.authenticate("jwt-authentication", {
+    session: false,
+    failureRedirect: "/login",
+});
 
-    static checkValidJWT = (req) => {
-        // Check if cookies exist
-        if (!req.cookies) {
-            return false;
-        }
-    
-        //Check if JWT cookie exists
-        if (!req.cookies.hasOwnProperty("jwt")) {
-            return false;
-        }
-    
-        //Get token from JWT
-        const token = req.cookies.jwt;
-    
-        //Verify token
-        const errors = jwt.verify(token, process.env.APP_SECRET, (err) => {
-            return err;
-        });
-    
-        return errors == null;
-    };
-}
-
-module.exports = Auth;
+module.exports = auth;
