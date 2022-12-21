badd968ccdc9b9aa03323af932e1234c26ce511f
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-12-21 11:35:14 +0100

    Changed sponsor image file

--- web/routes/api/sponsors/sponsor.api.js
+++ web/routes/api/sponsors/sponsor.api.js
@@ -2,21 +2,33 @@ const express = require("express");
 const router = express.Router();
 const SponsorController = require("../../../bin/db_sponsor_controller");
 const HSC = require("http-status-codes");
+const fs = require("fs");
+const path = require("path");
 
 /**
  * Get all sponsors from the database
  */
 router.get("/all", async (req, res) => {
     try {
-        const sponsors = await SponsorController.getAllSponsors();
+        let sponsors = await SponsorController.getAllSponsors();
+
         if (!sponsors || sponsors.length === 0) {
             return res
                 .status(HSC.StatusCodes.NOT_FOUND)
                 .json({ message: "No sponsors found!" });
         } else {
+            sponsors.forEach((sponsor) => {
+                const buffer = fs.readFileSync(
+                    path.join(__dirname, "../../../uploads/img/", sponsor.logo)
+                );
+                //Convert the data to base 64
+                sponsor.logo = Buffer.from(buffer).toString("base64");
+            })            
+
             return res.status(HSC.StatusCodes.OK).json(sponsors);
         }
     } catch (e) {
+        console.log(e); 
         return res
             .status(HSC.StatusCodes.INTERNAL_SERVER_ERROR)
             .json({ message: "Getting all routes failed" });
